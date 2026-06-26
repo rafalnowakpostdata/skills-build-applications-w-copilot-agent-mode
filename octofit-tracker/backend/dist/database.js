8d32dd999aa59c35a638a1401fca95bb70cf1ab8
rafalnowakpostdata <90577739+rafalnowakpostdata@users.noreply.github.com>
2026-06-26 10:08:42 +0000

    Step 3_v3

--- octofit-tracker/backend/dist/database.js
+++ octofit-tracker/backend/dist/database.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectToDatabase = exports.mongoUri = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
-exports.mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/octofit_db';
+const localMongoUri = 'mongodb://localhost:27017/octofit_db';
+exports.mongoUri = process.env.MONGO_URI || localMongoUri;
 const connectToDatabase = async () => mongoose_1.default.connect(exports.mongoUri);
 exports.connectToDatabase = connectToDatabase;
